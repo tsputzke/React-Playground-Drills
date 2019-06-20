d22f9bae7824702128de5232e46d864229a2ad00
Timothy Putzke <tsputzke@gmail.com>
2019-06-20 12:03:24 -0400

    setup leapyear test in src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip';
+
+// make 2 tooltips here and another inside the App directly
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='App'>
+      <Split className='left' flexBasis='2'>
+        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+        {/* make another tooltip directly inside the App */}
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/composition/Tooltip.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Tooltip.css';
+
+// function Tooltip(props) {
+//   return (
+//     <span className='Tooltip'>
+//       <span 
+//         className='Tooltip-content'
+//         style={{ color: props.color }}
+//       >  
+//         {props.children}
+//       </span>
+//       <div className='Tooltip-message'>
+//         {props.message}
+//       </div>
+//     </span>
+//   )
+// }
+
+class TooltipClass extends React.Component {
+  static defaultProps = {
+    color: '#01A800', // green
+  };
+  render() {
+    return (
+      <span className='Tooltip'>
+        <span
+          className='Tooltip-content'
+          style={{ color: this.props.color }}
+        >
+          {this.props.children}
+        </span>
+        <div className='Tooltip-message'>
+          {this.props.message}
+        </div>
+      </span>
+    )
+  }
+}
+
+export default TooltipClass;
