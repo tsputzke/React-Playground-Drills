84a3c2282e69bd935694777c7b6c3656ad6e2c57
Timothy Putzke <tsputzke@gmail.com>
2019-06-22 08:41:30 -0400

    Test smoke and snapshot testing on messages.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import React from 'react';
-import Split from './composition/Split';
+import React, { Component } from 'react';
 import './App.css';
-import Tooltip from './composition/Tooltip';
+import Messages from './Messages';
 
-// make 2 tooltips here and another inside the App directly
-const firstTooltip = (
-  <Tooltip color='hotpink' message='tooltip message'>
-   ipsum
-  </Tooltip>
-)
-const secondTooltip = (
-  <Tooltip color='#126BCC' message='another tooltip message'>
-    officiis
-  </Tooltip>
-)
-
-function App() {
-  return (
-    <main className='App'>
-      <Split className='left' flexBasis='2'>
-        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
-        {/* make another tooltip directly inside the App */}
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>
+        <Messages name="Messages" unread={0}/>
+        <Messages name="Notifications" unread={10}/>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
