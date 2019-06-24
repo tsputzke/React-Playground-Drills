25d9f937fa009fd2cd1940d50fac7de8644c631e
Timothy Putzke <tsputzke@gmail.com>
2019-06-23 20:09:10 -0400

    Implement conditional rendering on Tabs component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import Counter from './state/Counter';
 import HelloWorld from './state-drills/HelloWorld'
 import Bomb from './state-drills/Bomb'
 import RouletteGun from './state-drills/RouletteGun';
+import Tabs from './state/Tabs';
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
 class App extends Component {
   render() {
@@ -19,6 +29,7 @@ class App extends Component {
         <HelloWorld />
         <Bomb />
         <RouletteGun  bulletInChamber={3} />
+        <Tabs tabs={tabsProp}/>
       </div>
     );
   }
