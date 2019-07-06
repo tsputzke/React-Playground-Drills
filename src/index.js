dbf1016243f61f6037949a6989aa8d3de2858f26
Timothy Putzke <tsputzke@gmail.com>
2019-07-06 10:09:08 -0400

    Add Context React App -AppLang

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './paltrows-power-toes/App';
+import AppLang from './lang-context/AppLang'
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
-  document.getElementById('root')
-);
+ReactDOM.render(<AppLang />, document.getElementById('root'));
